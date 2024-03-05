0d73f887fdfe2941acdd36ec92f2bea26962cfe0
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-05 14:54:54 +0100

    spooky scary html+css+js skeleton

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//fornire il prezzo del biglietto sulla base di kilometraggio ed età, arrotondato a 2 cifre dopo la virgola
+//0.21 e/km
+//20% di sconto per i minorenni
+//40% di sconto per gli over 65
