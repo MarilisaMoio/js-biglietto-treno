765509a48e6e01cab4ed882a6eb9f15f317351af
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-05 16:04:05 +0100

    chore: add missing semicolon

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@
 //40% di sconto per gli over 65
 
 //raccolta dei dati
-const travelKm = parseInt(prompt("Inserire qui i kilometri da percorrere"))
+const travelKm = parseInt(prompt("Inserire qui i kilometri da percorrere"));
 
-const userAge = parseInt(prompt("Inserire qui la propria età"))
+const userAge = parseInt(prompt("Inserire qui la propria età"));
 
 //prezzo base del biglietto
-const fare = 0.21
+const fare = 0.21;
 
-const basePrice = fare * travelKm
+const basePrice = fare * travelKm;
 
 //definire l'eventuale scontistica
-const teenFare = 20
+const teenFare = 20;
 
-const elderlyFare = 40
+const elderlyFare = 40;
 
 //definire il prezzo finale
-let finalPrice = basePrice
+let finalPrice = basePrice;
 
 if (userAge < 18) {
     finalPrice = basePrice / 100 * (100 - teenFare)
@@ -28,7 +28,7 @@ if (userAge < 18) {
 }
 
 //output
-document.getElementById("price-shown").innerHTML = finalPrice.toFixed(2)
+document.getElementById("price-shown").innerHTML = finalPrice.toFixed(2);
 
 //messaggio di scontistica ottenuta
 if (userAge < 18) {
